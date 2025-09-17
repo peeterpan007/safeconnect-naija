3e690fb63348ee9e2bd88e41be3d02248d86d21d
PeterPan007 <funkeyent3@gmail.com>
2025-09-17 23:10:45 +0100

    Update App.jsx, add map and footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Ads from "./components/Ads";
 import CommunityConnect from "./components/CommunityConnect";
 import NewsUpdate from "./components/NewsUpdates";
 import LocalEventsAndBusiness from "./components/LocalEventsBusiness";
-import logo from "./assets/Connect5.png"; // Import your logo
+import logo from "./assets/Connect4.jpg"; // Import your logo
 import "./App.css";
 
 const user = { id: "1", area: "NY", interests: ["security", "home services"] };
